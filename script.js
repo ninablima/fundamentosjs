97ec3062b2229d665bfa69ea0909ffbb905b519e
Geração Tech <geracaotech@digitalcollege.internal>
2025-05-06 16:25:06 -0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('ola');
+// let a = 10;
+// let b = 20;
+// let c;
+
+// c = a
+// a = b;
+// b = c;
+
+// console.log(`a = ${a}, b = ${b}`);
+
+let nomeUsuario = prompt("Qual seu nome?")
+let profissaoUsuario = prompt("Qual sua profissão? ")
+
+alert(`Olá ${nomeUsuario}! Que interessante saber que você trabalha como ${profissaoUsuario}.`);
+
